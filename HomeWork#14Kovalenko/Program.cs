51273fff74c3ad64f1496c234fdb7728361c1fb1
Oleksii Kovalenko <oleksii.kovalenko31@gmail.com>
2024-12-27 19:47:56 +0100

    Добавьте файлы проекта.

---
+++ HomeWork#14Kovalenko/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork_14Kovalenko
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
